b7173bc7ce3767ab825314ecbcc1c6e2f841c025
MarshallTian <yaoling303@163.com>
2014-12-29 15:16:43 +0800

    Test

--- src/typeinfo/SimpleDynamicProxy.java
+++ src/typeinfo/SimpleDynamicProxy.java
@@ -1,6 +1,7 @@
 package typeinfo;
 
 import java.lang.reflect.*;
+import static net.fields.util.Print.*;
 /**
  * Created by Administrator on 2014/12/17 0017.
  */
@@ -11,6 +12,29 @@ class DynamicProxyHandler implements InvocationHandler {
         this.proxied = proxied;
     }
     public Object
+    invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        print("**** proxy: " + proxy.getClass() + ", method: " + method + ", args: " + args);
+        if (args != null) {
+            for (Object arg : args)
+                print("  " + args);
+        }
+        return method.invoke(proxied, args);
+    }
 }
+
 public class SimpleDynamicProxy {
+    public static void consumer(Interface iface) {
+        iface.doSomething();
+        iface.somethingElse("bonobo");
+    }
+    public static void main(String[] args) {
+        RealObject real = new RealObject();
+        consumer(real);
+        Interface proxy = (Interface)Proxy.newProxyInstance(
+                Interface.class.getClassLoader(),
+                new Class[]{ Interface.class },
+                new DynamicProxyHandler(real));
+        consumer(proxy);
+    }
+
 }
